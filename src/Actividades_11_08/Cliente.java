8eb658669245ef0bffda3b32e8c34f7ee32a8801
HairoWolf <javiertg2002@gmail.com>
2022-11-10 09:56:30 +0100

    Commit Actividades de clase 10-11 trabajo con sockets, entornos grafico y comunicaciones UDP

--- src/Actividades_11_08/Cliente.java
+++ src/Actividades_11_08/Cliente.java
@@ -10,6 +10,8 @@ public class Cliente {
     private Socket cliente;
     private String mensaje;
     private DataOutputStream salida;
+    private DataInputStream entrada;
+    private String mensajeRecibido;
 
     public static void main(String[] args) {
         Cliente c = new Cliente();
@@ -21,13 +23,33 @@ public class Cliente {
      */
     public void initClient(){
         try {
-            cliente = new Socket("192.168.1.230", PUERTO);
-            System.out.println("Write message.....");
-            Scanner sc = new Scanner(System.in);
-            mensaje = sc.nextLine();
-            salida = new DataOutputStream(cliente.getOutputStream());
-            salida.writeUTF(mensaje);
-            System.out.println("Message send");
+            boolean loop = true;
+            cliente = new Socket("localhost", PUERTO);
+            while(loop) {
+                System.out.println("Write message..... (salir : para terminar la comunicacion)");
+                Scanner sc = new Scanner(System.in);
+                mensaje = sc.nextLine();
+                salida = new DataOutputStream(cliente.getOutputStream());
+                entrada = new DataInputStream(cliente.getInputStream());
+                if(mensaje.equals("salir")){
+                    loop = false;
+                    salida.writeUTF(mensaje);
+                }
+                else {
+                    salida.writeUTF(mensaje);
+                    System.out.println("Message send");
+                    System.out.println("===========================");
+                    mensajeRecibido = entrada.readUTF();
+                    if(mensajeRecibido.equals("salir")){
+                        loop = false;
+                    }
+                    else {
+                        System.out.println("El servidor dice.... " + mensajeRecibido);
+                        System.out.println("===========================");
+                    }
+                }
+            }
+            entrada.close();
             salida.close();
             cliente.close();
         } catch (UnknownHostException e) {
--- src/Actividades_11_08/Servidor.java
+++ src/Actividades_11_08/Servidor.java
@@ -11,6 +11,8 @@ public class Servidor {
     private Socket socket;
     private DataInputStream entrada;
     private String mensajeRecibido;
+    private String mensaje;
+    private DataOutputStream salida;
 
     public static void main(String[] args) {
         Servidor s = new Servidor();
@@ -21,14 +23,36 @@ public class Servidor {
         try {
             server = new ServerSocket(PUERTO);
             socket = new Socket();
+            boolean loop = true;
 
             System.out.println("Waiting connection....");
             socket = server.accept();
-            entrada = new DataInputStream(socket.getInputStream());
             System.out.println("Cliente connected....");
-            mensajeRecibido = entrada.readUTF();
-            System.out.println("El cliente dice.... " + mensajeRecibido);
 
+            while(loop) {
+                entrada = new DataInputStream(socket.getInputStream());
+                salida = new DataOutputStream(socket.getOutputStream());
+                mensajeRecibido = entrada.readUTF();
+                if(mensajeRecibido.equals("salir")){
+                    loop = false;
+                }
+                else {
+                    System.out.println("El cliente dice.... " + mensajeRecibido);
+                    System.out.println("===========================");
+                    System.out.println("Write message..... (salir : para terminar la comunicacion)");
+                    Scanner sc = new Scanner(System.in);
+                    mensaje = sc.nextLine();
+                    if (mensaje.equals("salir")) {
+                        loop = false;
+                        salida.writeUTF(mensaje);
+                    } else {
+                        salida.writeUTF(mensaje);
+                        System.out.println("Message send");
+                        System.out.println("===========================");
+                    }
+                }
+            }
+            salida.close();
             entrada.close();
             socket.close();
             server.close();
